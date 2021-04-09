c146710a1cbc7b05f96b2d2229081d867bf14173
huangjuhua <952172047@qq.com>
2021-04-09 18:28:10 +0800

    init

---
+++ ewa.config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  // 公用代码库 (node_modules 打包生成的文件)名称，默认为 vendors.js
+  commonModuleName: 'vendors.js',
+
+  // 通用模块匹配模式，默认为 /[\\/](node_modules|utils|vendor)[\\/].+\.js/
+  // 如需添加多个文件夹，可自定义正则，如 /[\\/](node_modules|utils|custom_dirname)[\\/].+\.js/
+  commonModulePattern: /[\\/](node_modules|utils|vendor)[\\/].+\.js/,
+
+  // 是否简化路径，作用于 page 和 component，如 index/index.wxml=> index.wxml，默认为 false
+  simplifyPath: false,
+
+  // 文件夹快捷引用
+  aliasDirs: [
+    'apis',
+    'assets',
+    'constants',
+    'utils'
+  ],
+
+  // 需要拷贝的文件类型
+  copyFileTypes: [
+    'png',
+    'jpeg',
+    'jpg',
+    'gif',
+    'svg',
+    'ico',
+    'webp',
+    'apng'
+  ],
+
+  // webpack loader 规则
+  rules: [],
+
+  // webpack 插件
+  plugins: [],
+
+  // 开发环境下是否自动清理无用文件，默认为 true
+  autoCleanUnusedFiles: true,
+
+  // css 解析器，sass 或者 less，默认为 sass
+  cssParser: 'sass',
+
+  // 是否开启 hashed module id
+  hashedModuleIds: true,
+
+  // 是否开启缓存，默认为 true
+  cache: true,
+
+  // 嫌不够灵活？直接修改 webpack 配置
+  webpack: function(config) {
+    return config;
+  }
+};
---
+++ src/utils/request.js
@@ -0,0 +1,7 @@
+const config = require('../../config');
+
+function http(){
+  return config;
+}
+
+export default http;
