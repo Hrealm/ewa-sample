a2c0c0ef9c9ac173b9fade93b3207c0ad99b4aa0
huangjuhua <952172047@qq.com>
2021-04-13 18:58:30 +0800

    updated utils

--- src/utils/httpUtils.js
+++ src/utils/httpUtils.js
@@ -1,14 +1,27 @@
 const config = require('../../config');
 
-export const request = function(path, method, data){
+export const request = function (path, method, data, header) {
   return new Promise((resolve, reject) => {
-
+    wx.request({
+      url: config.BASE_API + path,
+      header: header || {},
+      method: method,
+      data: data,
+      success: function (res) {
+        if (res.statusCode == 200) {
+          resolve(res.data)
+        }
+      },
+      fail: function (res) {
+        reject(res);
+      }
+    })
   })
 }
 
 
-export function http(){
-  
+export function login() {
+
   return config;
 }
 
